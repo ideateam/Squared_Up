fab3c3316c2d0f47a72e02efda4ad8b478ff733e
Derek <1157880324@qq.com>
2017-05-21 14:46:39 +0800

    upload

---
+++ Squared_Up_fei/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Squared_Up_fei
+//
+//  Created by Derek on 2017/5/21.
+//  Copyright © 2017年 Derek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Squared_Up_feiTests/Squared_Up_feiTests.m
@@ -0,0 +1,39 @@
+//
+//  Squared_Up_feiTests.m
+//  Squared_Up_feiTests
+//
+//  Created by Derek on 2017/5/21.
+//  Copyright © 2017年 Derek. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Squared_Up_feiTests : XCTestCase
+
+@end
+
+@implementation Squared_Up_feiTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
