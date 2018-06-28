393c95742ae6889482d62a69547d942aafd81e09
风中的芦苇哥 <1157880324@qq.com>
2018-06-28 17:31:39 +0800

    fix

--- Squared_Up_fei/SquareUpFBC.h
+++ Squared_Up_fei/SquareUpFBC.h
@@ -20,7 +20,6 @@
 @property (nonatomic,assign) double squareness_width;//矩形宽
 @property (nonatomic,assign) double squareness_height;//矩形高
 @property (nonatomic,assign) double squareness_padding_to_top;//矩形距离上方的距离
-//@property (nonatomic,assign) double squareness_padding_to_bottom;//矩形距离下方的距离 (为方便更加灵活可定制，去掉了这个参数的数值设定)
 @property (nonatomic,assign) double squareness_padding_to_left;//矩形距离最左侧的距离
 @property (nonatomic,assign) double squareness_padding_to_right;//矩形距离右侧的距离
 @property (nonatomic,assign) double squareness_avage_distence_horizental;//水平方向每一个的平均距离
@@ -46,7 +45,10 @@
  *  squareness_avage_distence_vertical;//垂直方向的每一个的平均距离
  *
  */
-
+//可自由定义距离左侧的距离，可自由定义右侧的距离，可自由定义格子的宽、高，可自由定义距离最上面的距离
 -(instancetype)initWithBaseFrame:(CGRect)frame andNum_of_squareness:(int)num_of_squareness andNum_of_squareness_horizontal:(int)num_of_squareness_horizontal andSquareness_width:(double)squareness_width andSquareness_height:(double)squareness_height andSquareness_padding_to_top:(double)squareness_padding_to_top andSquareness_padding_to_left:(double)squareness_padding_to_left andSquareness_padding_to_right:(double)squareness_padding_to_right andSquareness_avage_distence_horizental:(double)squareness_avage_distence_horizental andSquareness_avage_distence_vertical:(double)squareness_avage_distence_vertical;
+
+//水平方向平均间隔，包括距离左侧、右侧、格子之间的距离都是一样的。不可定义格子的宽（自动调节宽），可自由定义距离最上面的距离
+-(instancetype)initWithBaseFrame:(CGRect)frame andNum_of_squareness:(int)num_of_squareness andNum_of_squareness_horizontal:(int)num_of_squareness_horizontal andSquareness_width:(double)squareness_width andSquareness_height:(double)squareness_height andSquareness_padding_to_top:(double)squareness_padding_to_top andSquareness_avage_distence_horizental:(double)squareness_avage_distence_horizental andSquareness_avage_distence_vertical:(double)squareness_avage_distence_vertical;
 
 @end
--- Squared_Up_fei/ViewController.m
+++ Squared_Up_fei/ViewController.m
@@ -24,14 +24,24 @@
     
     [self createSquare];
     
+    //[self createSquare2];
+    
 }
 -(void)createSquare{
     
-    SquareUpFBC *square = [[SquareUpFBC alloc] initWithBaseFrame:CGRectMake(0, 100, screen_width, 300) andNum_of_squareness:24 andNum_of_squareness_horizontal:7 andSquareness_width:40 andSquareness_height:40 andSquareness_padding_to_top:30 andSquareness_padding_to_left:20 andSquareness_padding_to_right:20 andSquareness_avage_distence_horizental:10 andSquareness_avage_distence_vertical:20];
+    SquareUpFBC *square = [[SquareUpFBC alloc] initWithBaseFrame:CGRectMake(0, 100, screen_width, 300) andNum_of_squareness:24 andNum_of_squareness_horizontal:4 andSquareness_width:60 andSquareness_height:60 andSquareness_padding_to_top:30 andSquareness_padding_to_left:40 andSquareness_padding_to_right:40 andSquareness_avage_distence_horizental:10 andSquareness_avage_distence_vertical:20];
     square.delegate = self;
     square.backgroundColor = [UIColor orangeColor];
     
     [self.view addSubview:square];
+}
+-(void)createSquare2{
+    
+    SquareUpFBC *square2 = [[SquareUpFBC alloc] initWithBaseFrame:CGRectMake(0, 100, screen_width, 300) andNum_of_squareness:24 andNum_of_squareness_horizontal:5 andSquareness_width:30 andSquareness_height:80 andSquareness_padding_to_top:20 andSquareness_avage_distence_horizental:30 andSquareness_avage_distence_vertical:20];
+    square2.delegate = self;
+    square2.backgroundColor = [UIColor orangeColor];
+    
+    [self.view addSubview:square2];
 }
 -(void)whichViewIsTap:(UITapGestureRecognizer *)tapGestureRecognizer{
     
